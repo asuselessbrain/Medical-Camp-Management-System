a2b07ffd50a14d3814ad817a28f892300e495f00
Arfan Ahmed <arfan18@cse.pstu.ac.bd>
2025-07-04 20:40:26 +0600

    add: developer information and cors origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,29 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors'
 
-const app = express()
+const app = express();
+
+
+app.use(express.json());
+app.use(cors({origin: ['http://localhost:5173', 'http://localhost:5174']}))
 
 app.get('/', (req: Request, res: Response) => {
   res.send({
     name: "Medical Camp Management System",
-    developeBy: "Arfan Ahmed",
-    date: new Date().toLocaleDateString(),
+    developedBy: {
+      name: "Arfan Ahmed",
+      designation: "Student, 7th Semester",
+      faculty: "Faculty of Computer Science and Engineering (CSE)",
+      institution: "Patuakhali Science and Technology University",
+      email: "arfan18@cse.pstu.ac.bd",
+      github: "https://github.com/asuselessbrain",
+      linkedin: "https://linkedin.com/in/arfan-ahmed40"
+    },
+    startDate: new Date("2025-07-01").toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    })
   })
 })
 
