8583d316648e7ee0a28e6bad4c53d725fc6424e6
Arfan Ahmed <arfan18@cse.pstu.ac.bd>
2025-07-08 22:11:46 +0600

    add: login auth services

---
+++ server/src/app/modules/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { StatusCodes } from "http-status-codes"
+import { AppError } from "../../error/appError"
+import { ILogin } from "../../types/types"
+import { User } from "../user/user.model"
+import bcrypt from 'bcrypt'
+
+const login = async(payload: ILogin)=>{
+    const isUserExist = await User.findOne({email: payload?.email})
+
+    if(!isUserExist){
+        throw new AppError(StatusCodes.NOT_FOUND, "User does not exist")
+    }
+
+    const isPasswordMatched = await bcrypt.compare(payload?.password, isUserExist?.password)
+
+    if(!isPasswordMatched){
+        throw new AppError(StatusCodes.UNAUTHORIZED, "Wrong email or password!")
+    }
+
+    console.log(payload)
+}
