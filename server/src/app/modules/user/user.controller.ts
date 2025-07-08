5215eb7ef453dbce22f35aebd28e309b661738e1
Arfan Ahmed <arfan18@cse.pstu.ac.bd>
2025-07-08 19:47:10 +0600

    add: get all doctors from db controller

--- server/src/app/modules/user/user.controller.ts
+++ server/src/app/modules/user/user.controller.ts
@@ -4,6 +4,7 @@ import { CatchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/response";
 import { StatusCodes } from "http-status-codes";
 
+// create patient
 const createPatient = CatchAsync(async (req: Request, res: Response) => {
     const { email, password, fullName, phoneNumber, gender, dob, address } = req.body
     const userData = { email, password, fullName, phoneNumber, gender, dob, address };
@@ -12,12 +13,13 @@ const createPatient = CatchAsync(async (req: Request, res: Response) => {
     sendResponse(res, StatusCodes.CREATED, "Patient created Successfully", createPatient)
 })
 
+// get all patient
 const getAllPatient = CatchAsync(async(req: Request, res: Response) => {
     const result = await userServices.allPatientsFromDB();
     sendResponse(res, StatusCodes.OK, "Patient retrive succefully", result);
 })
 
-
+// create doctor
 const createDoctor = CatchAsync(async (req: Request, res: Response) => {
     const { email, password, fullName, phoneNumber, gender, dob, profileImg, address, medicalRegNo, specialization, yearOfExperience, currentHospitalOrClinic, degrees, medicalLicenceDocuments } = req.body;
 
@@ -30,8 +32,15 @@ const createDoctor = CatchAsync(async (req: Request, res: Response) => {
 
 })
 
+// get all doctor
+const getAllDoctor = CatchAsync(async(req: Request, res: Response) => {
+    const result = await userServices.allDoctorsFromDB();
+    sendResponse(res, StatusCodes.OK, "Doctors retrive succefully", result);
+})
+
 export const userControllers = {
     createPatient,
     createDoctor,
-    getAllPatient
+    getAllPatient,
+    getAllDoctor
 }
